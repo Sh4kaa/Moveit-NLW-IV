be2277af1a7d2d9d1e64df0a199f4625a4db295b
Dario <darioo_dario@hotmail.com>
2021-02-27 00:17:43 -0300

    challengebox and context

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/ChallengeBox.module.css";
+
+export function ChallengeBox() {
+  const hasActiveChallenge = true;
+  const contexto = useContext(ChallengesContext);
+  console.log(contexto);
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 xp</header>
+
+          <main>
+            <img src="icons/body.svg" />
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos</p>
+          </main>
+
+          <footer>
+            <button type="button" className={styles.challengeFailedButton}>
+              Falhei
+            </button>
+
+            <button type="button" className={styles.challengeSucceededButton}>
+              Completei
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo para receber desafios</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up" />
+            Avance de level completando desafios
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
+import Head from "next/head";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from "next/head";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <head>
+      <Head>
         <title>Inicio | move.it</title>
-      </head>
+      </Head>
 
       <ExperienceBar />
 
@@ -22,7 +22,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
