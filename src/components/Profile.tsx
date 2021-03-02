4f44e975f04bcda2b047c826c8f0a1245e0df057
Dario <darioo_dario@hotmail.com>
2021-03-02 03:21:39 -0300

    correcao modulos

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { ChallengesContext } from "../contexts/ChallengesContext";
-import styles from "../styles/components/profile.module.css";
+import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
   const { level } = useContext(ChallengesContext);
